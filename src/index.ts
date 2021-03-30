23a615d5e65ae22999ebdd711ca54a6a2052c3d7
yijie4188 <yijie4188@gmail.com>
2021-03-30 20:42:35 +0800

    (feat) [config]: 保留注释

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 /**
- * @desc   入口 index.ts
- * @author yijie
- * @date   2021-03-30 07:40
- * @notes  2021-03-30 07:40 yijie 创建了 index.ts 文件
+ * print hello to param
+ *
+ * @param str the string after hello
+ *
+ * @return 修改后的字符串
  */
-
 export default (str: string): string => `hello ${str}`
