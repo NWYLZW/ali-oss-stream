572ec5cdbba97782cda4bbea4a777ea4a7d563aa
yijie4188 <yijie4188@gmail.com>
2021-03-31 10:51:59 +0800

    (refactor) [test]: let config.js to config.ts

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from '@jest/types'
+import * as path from "path"
+
+const config: Config.InitialOptions = {
+  verbose: true,
+  moduleNameMapper: {
+    '@ali-oss-stream/(.*)': path.join(__dirname, './src/$1'),
+    '@ali-oss-stream': path.join(__dirname, './src/index')
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: [ 'ts', 'json' ],
+};
+export default config;
