14cad25ef845395f9d3dc5232c1dfc8b866eee8e
yijie4188 <yijie4188@gmail.com>
2021-04-01 18:05:19 +0800

    (doc) [helper]: add many documents

--- src/helpers/ImgHelper.ts
+++ src/helpers/ImgHelper.ts
@@ -2,40 +2,64 @@ import Stream from '../core/stream'
 import Action from '../core/action'
 import { Helper } from './index'
 
-// [阿里oss参考文档](https://help.aliyun.com/document_detail/44688.htm?spm=a2c4g.11186623.2.8.5b7ff2ee8dxQ8X#title-dz0-c5s-ulp)
-export type ResizeMode =
-// 等比缩放，缩放图限制为指定w与h的矩形内的最大图片。
-  'lfit' |
-  // 等比缩放，缩放图为延伸出指定w与h的矩形框外的最小图片。
-  'mfit' |
-  // 将原图等比缩放为延伸出指定w与h的矩形框外的最小图片，之后将超出的部分进行居中裁剪。
-  'fill' |
-  // 将原图缩放为指定w与h的矩形内的最大图片，之后使用指定颜色居中填充空白部分。
-  'pad' |
-  // 固定宽高，强制缩放。
-  'fixed'
+/**
+ * [阿里oss参考文档](https://help.aliyun.com/document_detail/44688.htm?spm=a2c4g.11186623.2.8.5b7ff2ee8dxQ8X#title-dz0-c5s-ulp)
+ *
+ * - 'lfit'  等比缩放，缩放图限制为指定w与h的矩形内的最大图片。
+ * - 'mfit'  等比缩放，缩放图为延伸出指定w与h的矩形框外的最小图片。
+ * - 'fill'  将原图等比缩放为延伸出指定w与h的矩形框外的最小图片，之后将超出的部分进行居中裁剪。
+ * - 'pad'   将原图缩放为指定w与h的矩形内的最大图片，之后使用指定颜色居中填充空白部分。
+ * - 'fixed' 固定宽高，强制缩放。
+ */
+export type ResizeMode = 'lfit' | 'mfit' | 'fill' | 'pad' | 'fixed'
 
+/**
+ * 调整大小配置参数
+ *
+ * - 'm' 指定缩放的模式。
+ * - 'w' 调整宽度。
+ * - 'h' 调整高度。
+ * - 'l' 指定目标缩放图的最长边。
+ * - 's' 指定目标缩放图的最短边。
+ * - 'limit' 指定当目标缩放图大于原图时是否进行缩放。
+ * - 'color' 当缩放模式选择为pad（缩放填充）时，可以设置填充的颜色。
+ */
 type resizeOptions = {
   [key: string]: number | string | boolean | undefined
-  // 指定缩放的模式。
   m?: ResizeMode,
   w?: number, h?: number,
-  // 指定目标缩放图的最长边。
   l?: number,
-  // 指定目标缩放图的最短边。
   s?: number,
-  // 指定当目标缩放图大于原图时是否进行缩放。
   limit?: boolean,
-  // 当缩放模式选择为pad（缩放填充）时，可以设置填充的颜色。
   color?: string
 }
 
+/**
+ * [可选择的action类型](https://help.aliyun.com/document_detail/183902.html?spm=a2c4g.11186623.6.746.68eb7fd8lmUm0t#title-cya-7jq-mzs)
+ *
+ * - 'format'          将图片转换为HEIF或WebP M6等高压缩比格式。转换图片格式。
+ * - 'resize'          将图片缩放至指定大小。
+ * - 'circle'          以图片中心点为圆心，裁剪出指定大小的圆形图片。
+ * - 'crop'            裁剪指定大小的矩形图片。
+ * - 'indexcrop'       按指定x或y轴的大小切分图片，之后选取其中一张图片。
+ * - 'rounded-corners' 按指定圆角大小将图片裁剪成圆角矩形。
+ * - 'auto-orient'     将携带旋转参数的图片进行自适应旋转。
+ * - 'rotate'          按指定角度以顺时针方向旋转图片。
+ * - 'blur'            对图片进行模糊处理。
+ * - 'bright'          调整图片亮度。
+ * - 'sharpen'         对图片进行锐化处理。
+ * - 'contrast'        调整图片对比度。
+ * - 'interlace'       将JPG格式的图片调整为渐进显示。
+ * - 'quality'         调整JPG和WebP格式图片的质量。
+ * - 'watermark'       为图片添加图片或文字水印。
+ * - 'average-hue'     获取图片主色调。
+ * - 'info'            获取图片信息，包括基本信息、EXIF信息。
+ */
 type actions =
-  '' |
-  // 调整大小
-  'resize' |
-  // 图片水印
-  'watermark'
+  'format' | 'resize' | 'circle' | 'crop' | 'indexcrop' |
+  'rounded-corners' | 'auto-orient' | 'rotate' | 'blur' |
+  'bright' | 'sharpen' | 'contrast' | 'interlace' | 'quality' |
+  'watermark' | 'average-hue' | 'info'
 
 class ImageAction extends Action<actions> {}
 
@@ -50,6 +74,12 @@ export class ImageStream extends Stream<string> {
     }
   }
 
+  /**
+   * [阿里oss 图片缩放参考文档](https://help.aliyun.com/document_detail/44688.htm?spm=a2c4g.11186623.2.8.5b7ff2ee8dxQ8X#title-dz0-c5s-ulp)
+   *
+   * @param option - 缩放配置参数
+   * @return ImageStream
+   */
   resize (option: resizeOptions): ImageStream {
     const params = []
     for (const optionKey in option) {
